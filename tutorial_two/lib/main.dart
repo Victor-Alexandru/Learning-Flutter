c043ef1a1a8e9b0667955e29dd17be6673c89691
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-12-27 12:05:15 +0200

    Interactivity and statefull added

--- tutorial_two/lib/main.dart
+++ tutorial_two/lib/main.dart
@@ -22,16 +22,55 @@ Widget titleSection = Container(
           ],
         ),
       ),
-      Column(children: <Widget>[Icon(Icons.star, color: Colors.red[500])]),
-      Column(children: <Widget>[
-        Text(
-          '41',
-          style: TextStyle(fontSize: 20.0),
-        )
-      ]),
+      FavoriteWidget(), 
     ],
   ),
 );
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
 
 void main() => runApp(MyApp());
 
@@ -70,7 +109,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Image.asset(
               'lake.jpg',
@@ -80,6 +119,7 @@ class MyApp extends StatelessWidget {
             ),
             titleSection,
             buttonSection,
+            textSection
           ],
         ),
       ),
